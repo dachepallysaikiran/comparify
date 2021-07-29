bbc66641bb419898a4a94d40af058e9a1862b2b2
Dachepally <dachepallysaikiran@gmail.com>
2021-07-29 16:16:51 +0530

    made a change

--- comparify/manage.py
+++ comparify/manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+#made a change
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'comparify.settings')
